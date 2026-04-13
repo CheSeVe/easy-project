3beb0240520d33653c7b58434f503a3aacfa0451
CheSeVe <z.groodson@gmail.com>
2026-04-13 14:39:38 +0400

    add initial Spring Boot starter

---
+++ src/main/java/ru/cheseve/easyproject/EasyProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.cheseve.easyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/cheseve/easyproject/EasyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cheseve.easyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
